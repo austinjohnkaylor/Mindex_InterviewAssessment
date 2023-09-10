8ec6b88356ca56a993648cdc46482f17e17e4981
Austin Kaylor <austinjohnkaylor@outlook.com>
2023-09-09 23:02:49 -0400

    Adding a new controller for the Reportingstructure model

---
+++ CodeChallenge/Controllers/ReportingStructureController.cs
@@ -0,0 +1,32 @@
+using CodeChallenge.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CodeChallenge.Controllers;
+
+[ApiController]
+[Route("api/employee/{id}/reporting-structure")]
+public class ReportingStructureController : ControllerBase
+{
+    private readonly ILogger _logger;
+    private readonly IReportingStructureService _reportingStructureService;
+
+    public ReportingStructureController(ILogger logger, IReportingStructureService reportingStructureService)
+    {
+        _logger = logger;
+        _reportingStructureService = reportingStructureService;
+    }
+
+    [HttpGet]
+    public IActionResult GetReportingStructure(string id)
+    { 
+        _logger.LogDebug($"Received employee reporting structure get request for '{id}'");
+
+        var reportingStructure = _reportingStructureService.GetById(id);
+
+        if (reportingStructure == null)
+            return NotFound();
+
+        return Ok(reportingStructure);
+    }
+}
